fd268b30f067679965a7d4acf178ccd309c5b81b
Ritik Kumar <ritik.kumar006@gmail.com>
2020-07-08 02:32:37 +0530

    Added http requests to store and get recipes

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+
+import { Recipe } from '../recipe/recipe.model';
+import { RecipeService } from '../recipe/recipe.service';
+
+@Injectable({providedIn: "root"})
+
+export class DataStorageService{
+
+    constructor(private http: HttpClient, private recipeService: RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipe();
+        this.http.put('https://my-recipe-book-a9b70.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => console.log(response));
+    }
+
+    fetchRecipes(){
+        return this.http.get<Recipe[]>('https://my-recipe-book-a9b70.firebaseio.com/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            )
+    }
+
+}
